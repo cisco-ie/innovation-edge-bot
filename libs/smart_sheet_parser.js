93fedf74a78824637aca86b9296b35297b6e866b
Glitch (stone-duck) <none>
2018-01-15 22:34:11 +0000

    🌟🛣 Checkpoint libs/smart_sheet_parser.js:628884/1157

--- libs/smart_sheet_parser.js
+++ libs/smart_sheet_parser.js
@@ -3,6 +3,10 @@ const SHEET_ID = process.env.sheet_id;
 const SHEET_CACHE_KEY = 'IE_SHEET';
 const Cache = require('../store/bot_cache.js');
 
+let active_projects = [];
+let complete_projects = [];
+let potential_projects = [];
+
 // Initialize the client
 const client = require('smartsheet');
 const smartsheet = client.createClient({
@@ -12,6 +16,7 @@ const smartsheet = client.createClient({
 
 // Retrieves sheet and stores in bot cache
 const processSheet = () => {
+ 
   // The `smartsheet` variable now contains access to all of the APIs
   smartsheet.sheets.getSheet({id: SHEET_ID})
     .then(function(sheetInfo) {
@@ -22,33 +27,35 @@ const processSheet = () => {
           [column.id]: column.title
         });
       }, {});
-      // // This will create a basic object structure per column names dynamically
-      // const rowStructure = columnNames.reduce((acc, value) => {
-      //   acc[value] = '';
-      //   return acc;
-      // }, {});
     
+      // This will create a row that is formatted into a usable object
       const rows = sheetInfo.rows.map((row) => {
         const formattedRow = row.cells.reduce((acc, cell, index) => {
-          console.log(cell);
-          const key = columcell.columnId
-          return {};
-//             const columnKey = columnNames[index];
-// //            console.log('current key', columnKey);
-//             const rowCopy = Object.assign({}, acc, rowStructure);
-//             //console.log(cell);
-//             rowCopy[columnKey] = cell.value;
-//           //console.log(rowCopy);
-//             return rowCopy;
+          const key = columnMap[cell.columnId];
+          return Object.assign({}, acc, {
+            [key]: cell.value || 'N/A'
+          });
         }, {});
         return formattedRow;
       });
-  
+    
+      // Store in cache
       Cache.set(SHEET_CACHE_KEY, rows);
     })
     .catch(function(error) {
       console.log(error);
     });
+};
+
+const organizeProjects = (projects) => {
+  if (!projects) return;
+  projects.forEach(project => {
+    if (project.Status === 'Active') { active_projects.push(project) }
+    if (project.Status === 'Completed') { completed_projects.push(project) }
+    if (project.Status === 'Potential') { potential_projects.push(project) }
+  });
+  Cache.set(ACTIVE_PROJECT_KEY, active_projects);
+  Cache.set(COMPLETED_KEY, completedpro
 };
 
 // Check if it already exist in cache before processing
