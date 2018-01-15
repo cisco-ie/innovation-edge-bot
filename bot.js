5bbae012ce7305539651af02edec33b06b8042bf
Glitch (wakeful-city) <none>
2018-01-15 21:23:48 +0000

    Exporting to cisco-ie/spark-innovation-edge

--- bot.js
+++ bot.js
@@ -20,6 +20,8 @@ This is a sample Cisco Spark bot built with Botkit.
 var env = require('node-env-file');
 env(__dirname + '/.env');
 
+const t = require('./libs/smart_sheet_parser.js');
+
 
 if (!process.env.access_token) {
     console.log('Error: Specify a Cisco Spark access_token in environment.');
--- libs/smart_sheet_parser.js
+++ libs/smart_sheet_parser.js
@@ -1,27 +1,19 @@
+// Constants, let's move later
+const SHEET_ID = process.env.SECRET.sheet_id;
+
 // Initialize the client
 const client = require('smartsheet');
-const SHEET_ID = process.env.
 const smartsheet = client.createClient({
   accessToken: process.env.SECRET.smart_sheet_token
   logLevel: 'info'
 });
 
 // The `smartsheet` variable now contains access to all of the APIs
-
-// List all sheets
-smartsheet.sheets.listSheets()
-  .then(function (result) {
-    var sheetId = result.data[0].id;                // Choose the first sheet
-
-    // Load one sheet
-    smartsheet.sheets.getSheet({id: sheetId})
-      .then(function(sheetInfo) {
-        console.log(sheetInfo);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
+smartsheet.sheets.getSheet({id: SHEET_ID})
+    .then(function(sheetInfo) {
+      console.log(sheetInfo);
+    })
+    .catch(function(error) {
+      console.log(error);
+    });
+})
