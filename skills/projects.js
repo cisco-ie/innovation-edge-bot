a363f7b1485eac6573ea1e3be8fc76ebd20a2806
Glitch (stone-duck) <none>
2018-01-16 00:55:00 +0000

    😱🚊 Checkpoint skills/sample_events.js:628884/265 skills/projects.js:628884/3

--- skills/projects.js
+++ skills/projects.js
@@ -5,12 +5,12 @@ const sm = require('spark-messages');
 
 module.exports = function(controller) {
 
-    controller.hears(['projects'], 'direct_message,direct_mention', function(bot, message) {
+    controller.hears(['^projects'], 'direct_message,direct_mention', function(bot, message) {
         // Fetch projects now for a better experience and less delay
         smartSheetParser.update();
       
         bot.startConversation(message, function(err, convo) {
-            convo.ask('What projects would you like to see? \n1. All \n2. Completed \n3. Active \n4. Potential', function(response, convo) {
+            convo.ask('What projects would you like to see? \n1. All \n2. Completed \n3. Active \n4. Potential \n5. Inactive', function(response, convo) {
                 const projectMap = {
                   1: {
                     key: CONSTANTS.ALL,
@@ -36,7 +36,7 @@ module.exports = function(controller) {
         
                 const formatMessage = `Cool! Here are some projects:`;
                 const category = parseInt(response.text);
-                if (category < 5 && category > 0) {
+                if (category < 6 && category > 0) {
                   try {
                     let projects = Cache.get(projectMap[category].key);
                     if (!projects) {
@@ -48,7 +48,7 @@ module.exports = function(controller) {
                     stateError(convo);
                   }
                 } else {
-                  convo.say('Hrmm... I\'m not too sure what you are looking for, respond with either **(1 / 2 / 3 /4)**');
+                  convo.say('Hrmm... I\'m not too sure what you are looking for, respond with either **(1 / 2 / 3 / 4 / 5)**');
                 }
                 convo.next();
             });
--- skills/sample_events.js
+++ skills/sample_events.js
@@ -1,15 +1,10 @@
 module.exports = function(controller) {
 
-
   controller.on('bot_space_join', function(bot, message) {
-
-    bot.reply(message, 'I am the automateCaptain and Commander of the Innovation Edge, here to do your bidding.');
-
+    bot.reply(message, 'Beep! Boop! I am a bot serving as Captain and Commander of the Innovation Edge\'s Bot Division. Let me know if I can be of your assistance.');
   });
 
   controller.on('user_space_join', function(bot, message) {
-
     bot.reply(message, 'Hello, ' + message.raw_message.data.personDisplayName);
-
   });
 };
