5d8c8ee841bff2f03c6ddb2a5c7824e529468846
Glitch (stone-duck) <none>
2018-01-16 02:11:59 +0000

    🍚🔪 Checkpoint skills/jokes.js:628884/37 package.json:628884/1

--- skills/jokes.js
+++ skills/jokes.js
@@ -1,17 +1,12 @@
-var oneLinerJoke = require('one-liner-joke');
 
 module.exports = function(controller) {
     controller.studio.before('jokes', function(convo, next) {
         var joke = oneLinerJoke.getRandomJoke();
-      console.log(joke)
-        convo.setVar('joke', joke.body);
+        convo.say(joke.body);
       
         next();
     });
 
-    // define an after hook
-    // you may define multiple after hooks. they will run in the order they are defined.
-    // See: https://github.com/howdyai/botkit/blob/master/docs/readme-studio.md#controllerstudioafter
     controller.studio.after('jokes', function(convo, next) {
 
         console.log('AFTER: jokes');
