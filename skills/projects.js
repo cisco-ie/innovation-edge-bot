8e0b16b6c7b1c3e3c740b27fb77cd0ab0c7f3529
Glitch (stone-duck) <none>
2018-01-16 00:01:02 +0000

    😽🛫 Checkpoint skills/projects.js:628884/355 package.json:628884/9

--- skills/projects.js
+++ skills/projects.js
@@ -2,11 +2,14 @@ const Cache = require('../store/bot_cache.js');
 const CONSTANTS = require('../constants/index.js');
 const smartSheetParser = require('../libs/smart_sheet_parser.js');
 
+const retry = require('retry');
 const sm = require('spark-messages');
 
 module.exports = function(controller) {
 
     controller.hears(['projects'], 'direct_message,direct_mention', function(bot, message) {
+        // Fetch projects now for a better experience and less delay
+        
       
         bot.startConversation(message, function(err, convo) {
             convo.ask('What projects would you like to see? \n1. All \n2. Completed \n3. Active \n4. Potential', function(response, convo) {
@@ -37,7 +40,8 @@ module.exports = function(controller) {
                 const formatMessage = `Cool! Here are some projects:`;
                 const category = parseInt(response.text);
                 if (category < 5 && category > 0) {
-      
+                  convo.say(`Currently there are **${value.length}** ${projectMap[category].name} projects. Here are the projects: \n`);
+                  convo.say(listProjects(value));
                 } else {
                   convo.say('Hrmm... I\'m not too sure what you are looking for, respond with either **(1 / 2 / 3 /4)**');
                 }
@@ -47,17 +51,35 @@ module.exports = function(controller) {
     });
 };
 
-const fetchProjects () => {
-              try {
-                      let value = myCache.get(projectMap[category].key, true);
-                      smartSheetParser.fetchAndUpdate(
-                      if (!value) 
-                      convo.say(`Currently there are **${value.length}** ${projectMap[category].name} projects. Here are the projects: \n`);
-                      convo.say(listProjects(value));
-                  } catch(err) {
-                      convo.say('🔥 Houston, we got a problem. Please reach out to 👩‍🚒 Brandon Him (brhim@cisco.com) about this issue!');
-                  }
+const fetchProjects = (cb) => {
+  const operation = retry.operation();  
 }
+
+function faultTolerantResolve(address, cb) {
+  let operation = retry.operation();
+
+  operation.attempt(function(currentAttempt) {
+    
+    try {
+      let value = myCache.get(projectMap[category].key, true);
+      if (!value) {
+        smartSheetParser.fetchAndUpdate();
+        fetchProjects();
+      }
+    
+      return value;
+    } catch(err) {
+  }
+    dns.resolve(address, function(err, addresses) {
+
+      cb(err ? operation.mainError() : null, addresses);
+    });
+  });
+}
+
+faultTolerantResolve(function(err, addresses) {
+  console.log(err, addresses);
+});
 // All projects has a special format
 const listProjects = (projects) => {
    const message = projects.map(project => {
