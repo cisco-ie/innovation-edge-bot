1921250ba6beb63f5a5e1f60f906f40efb9b1e7d
Glitch (stone-duck) <none>
2018-01-17 20:34:39 +0000

    🍈🌅 Checkpoint process/pokemon.js:628884/486

--- process/pokemon.js
+++ process/pokemon.js
@@ -1,13 +1,20 @@
 const wordfilter = require('wordfilter');
 var PokeApi = require('pokeapi');
-const pokeNumber = 386;
+const pokeNumber = 800;
 var api = PokeApi.v1();
 
 module.exports = (bot, message, entities) => {
-  var randomNumber = Math.floor(Math.random() * 386);
-
-  api.get('pokemon', randomNumber).then(function(pokemon) {    
-    bot.say(message, {text:'', files:['http://myserver.com/file.pdf']});
+  var randomNumber = Math.floor(Math.random() * pokeNumber);
+  api.get('pokemon', randomNumber).then(function(pokemon) {
+    console.log(pokemon);
+    const messageObject = {
+      // text: `Have you heard of, ${pokemon.name}! ${pokemon.name} has **${pokemon.moves.length}** moves, and weighs ${pokemon.weight} lbs!`,
+      files:[
+        `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomNumber}.png`
+      ]
+    };
+    
+    bot.say(message, messageObject);
   }, function(err) {
       console.log('ERROR', err);
   });
