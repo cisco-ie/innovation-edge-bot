8600c4060a71324781e79a633b5903e37fbf4aa1
Glitch (stone-duck) <none>
2018-01-17 20:55:30 +0000

    🗽🏣 Checkpoint process/pokemon.js:628884/863 skills/events.js:628884/2

--- process/pokemon.js
+++ process/pokemon.js
@@ -3,7 +3,18 @@ const request = require('request');
 const pokeNumber = 800;
 
 module.exports = (bot, message, entities) => {
-  bot.reply(message, 'Sure! Looking in my Pokédex right now... 🤓');
+  
+  console.log(message);
+  const randomIntros = [
+    'Sure! Looking in my Pokédex right now... 🤓',
+    'Lucky for you, I\'m a Pokémon Master! Let me check my Pokédex...',
+    `I've caught ${pokeNumber} Pokémon thus far, let me show you one of my favorites..`,
+    'Prepared to see one of the coolest of em all',
+    'I love Pokémon! Let me tell you one of my favorites, hold on..',
+    'Pokémon is my middle name! Here\'s a cool one...'
+  ];
+  const intro = randomIntros[Math.floor(Math.random() * randomIntros.length)];
+  bot.reply(message, intro);
   var randomNumber = Math.floor(Math.random() * pokeNumber);
   request('http://pokeapi.co/api/v2/pokemon/' + randomNumber, function (error, response, body) {
     if (error) {
@@ -12,7 +23,7 @@ module.exports = (bot, message, entities) => {
     const pokemon = JSON.parse(body);
     const name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
     const messageObject = {
-      text: `### #${randomNumer} ${name} \n ${name} has **${pokemon.moves.length}** moves, and weighs ${pokemon.weight} lbs!`,
+      text: `### #${randomNumber} ${name} \n ${name} has **${pokemon.moves.length}** moves, and weighs ${pokemon.weight} lbs!`,
       files:[
         `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${randomNumber}.png`
       ]
