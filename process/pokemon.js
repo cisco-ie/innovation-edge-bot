1610098a68ea9fa6cf33c5cfd7d1bef398ecce3b
Glitch (stone-duck) <none>
2018-01-17 20:24:10 +0000

    👲🍌 Checkpoint process/pokemon.js:628884/24

--- process/pokemon.js
+++ process/pokemon.js
@@ -6,8 +6,8 @@ var api = PokeApi.v1();
 module.exports = (bot, message, entities) => {
   var randomNumber = Math.floor(Math.random() * 386);
 
-  api.get('pokemon', randomNumber).then(function(pokemon) {
-    bot.say(
+  api.get('pokemon', randomNumber).then(function(pokemon) {    
+    bot.say(message, {text:'', files:['http://myserver.com/file.pdf']});
   }, function(err) {
       console.log('ERROR', err);
   });
